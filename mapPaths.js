a1e4aab6d970ed67609ce8bde5c7cef190b13df4
Alexandre Sajus <alexandre.sajus@student-cs.fr>
2025-03-09 02:35:06 +0100

    :hammer: Restructures index into multiple files

---
+++ mapPaths.js
@@ -0,0 +1,179 @@
+// Define the paths from the selected map
+export const mapPaths = {
+    Albasrah: {
+        heightmap: 'data/heightmaps/albasrah_array.csv',
+        satellite: 'data/satmaps/albasrah_sat.png',
+        colormap: 'data/colormaps/albasrah_color.png',
+        contour: 'data/contourmaps/albasrah_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Anvil: {
+        heightmap: 'data/heightmaps/anvil_array.csv',
+        satellite: 'data/satmaps/anvil_sat.png',
+        colormap: 'data/colormaps/anvil_color.png',
+        contour: 'data/contourmaps/anvil_contour.png',
+        scaleX: 0.75,
+        scaleY: 0.75
+    },
+    Belaya: {
+        heightmap: 'data/heightmaps/belaya_array.csv',
+        satellite: 'data/satmaps/belaya_sat.png',
+        colormap: 'data/colormaps/belaya_color.png',
+        contour: 'data/contourmaps/belaya_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Blackcoast: {
+        heightmap: 'data/heightmaps/blackcoast_array.csv',
+        satellite: 'data/satmaps/blackcoast_sat.png',
+        colormap: 'data/colormaps/blackcoast_color.png',
+        contour: 'data/contourmaps/blackcoast_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Chora: {
+        heightmap: 'data/heightmaps/chora_array.csv',
+        satellite: 'data/satmaps/chora_sat.png',
+        colormap: 'data/colormaps/chora_color.png',
+        contour: 'data/contourmaps/chora_contour.png',
+        scaleX: 0.5,
+        scaleY: 0.5
+    },
+    Fallujah: {
+        heightmap: 'data/heightmaps/fallujah_array.csv',
+        satellite: 'data/satmaps/fallujah_sat.png',
+        colormap: 'data/colormaps/fallujah_color.png',
+        contour: 'data/contourmaps/fallujah_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Foolsroad: {
+        heightmap: 'data/heightmaps/foolsroad_array.csv',
+        satellite: 'data/satmaps/foolsroad_sat.png',
+        colormap: 'data/colormaps/foolsroad_color.png',
+        contour: 'data/contourmaps/foolsroad_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Goosebay: {
+        heightmap: 'data/heightmaps/goosebay_array.csv',
+        satellite: 'data/satmaps/goosebay_sat.png',
+        colormap: 'data/colormaps/goosebay_color.png',
+        contour: 'data/contourmaps/goosebay_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Gorodok: {
+        heightmap: 'data/heightmaps/gorodok_array.csv',
+        satellite: 'data/satmaps/gorodok_sat.png',
+        colormap: 'data/colormaps/gorodok_color.png',
+        contour: 'data/contourmaps/gorodok_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Harju: {
+        heightmap: 'data/heightmaps/harju_array.csv',
+        satellite: 'data/satmaps/harju_sat.png',
+        colormap: 'data/colormaps/harju_color.png',
+        contour: 'data/contourmaps/harju_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Kamdesh: {
+        heightmap: 'data/heightmaps/kamdesh_array.csv',
+        satellite: 'data/satmaps/kamdesh_sat.png',
+        colormap: 'data/colormaps/kamdesh_color.png',
+        contour: 'data/contourmaps/kamdesh_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Kohat: {
+        heightmap: 'data/heightmaps/kohat_array.csv',
+        satellite: 'data/satmaps/kohat_sat.png',
+        colormap: 'data/colormaps/kohat_color.png',
+        contour: 'data/contourmaps/kohat_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Kokan: {
+        heightmap: 'data/heightmaps/kokan_array.csv',
+        satellite: 'data/satmaps/kokan_sat.png',
+        colormap: 'data/colormaps/kokan_color.png',
+        contour: 'data/contourmaps/kokan_contour.png',
+        scaleX: 0.5,
+        scaleY: 0.5
+    },
+    Lashkar: {
+        heightmap: 'data/heightmaps/lashkar_array.csv',
+        satellite: 'data/satmaps/lashkar_sat.png',
+        colormap: 'data/colormaps/lashkar_color.png',
+        contour: 'data/contourmaps/lashkar_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Logar: {
+        heightmap: 'data/heightmaps/logar_array.csv',
+        satellite: 'data/satmaps/logar_sat.png',
+        colormap: 'data/colormaps/logar_color.png',
+        contour: 'data/contourmaps/logar_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Manicouagan: {
+        heightmap: 'data/heightmaps/manicouagan_array.csv',
+        satellite: 'data/satmaps/manicouagan_sat.png',
+        colormap: 'data/colormaps/manicouagan_color.png',
+        contour: 'data/contourmaps/manicouagan_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Mestia: {
+        heightmap: 'data/heightmaps/mestia_array.csv',
+        satellite: 'data/satmaps/mestia_sat.png',
+        colormap: 'data/colormaps/mestia_color.png',
+        contour: 'data/contourmaps/mestia_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Mutaha: {
+        heightmap: 'data/heightmaps/mutaha_array.csv',
+        satellite: 'data/satmaps/mutaha_sat.png',
+        colormap: 'data/colormaps/mutaha_color.png',
+        contour: 'data/contourmaps/mutaha_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Narva: {
+        heightmap: 'data/heightmaps/narva_array.csv',
+        satellite: 'data/satmaps/narva_sat.png',
+        colormap: 'data/colormaps/narva_color.png',
+        contour: 'data/contourmaps/narva_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Sumari: {
+        heightmap: 'data/heightmaps/sumari_array.csv',
+        satellite: 'data/satmaps/sumari_sat.png',
+        colormap: 'data/colormaps/sumari_color.png',
+        contour: 'data/contourmaps/sumari_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Tallil: {
+        heightmap: 'data/heightmaps/tallil_array.csv',
+        satellite: 'data/satmaps/tallil_sat.png',
+        colormap: 'data/colormaps/tallil_color.png',
+        contour: 'data/contourmaps/tallil_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    },
+    Yehorivka: {
+        heightmap: 'data/heightmaps/yehorivka_array.csv',
+        satellite: 'data/satmaps/yehorivka_sat.png',
+        colormap: 'data/colormaps/yehorivka_color.png',
+        contour: 'data/contourmaps/yehorivka_contour.png',
+        scaleX: 1,
+        scaleY: 1
+    }
+};
---
+++ script.js
@@ -0,0 +1,231 @@
+import { mapPaths } from './mapPaths.js';
+
+// Get the selected map from the URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+const selectedMap = urlParams.get('map') || 'Manicouagan';
+
+// Set the selected value in the dropdown
+document.getElementById('map-selector').value = selectedMap;
+
+// Add event listener to the map selector
+document.getElementById('map-selector').addEventListener('change', function() {
+    const selectedMap = this.value;
+    window.location.search = `?map=${selectedMap}`;
+});
+
+// Define a custom coordinate reference system
+var crs = L.extend({}, L.CRS.Simple, {
+    transformation: new L.Transformation(1, 0, -1, 0)
+});
+
+// Initialize the map with the custom coordinate reference system
+var map = L.map('map', {
+    crs: crs,
+    minZoom: -5
+});
+
+var heightArray = [];
+var clickX = null;
+var clickY = null;
+var scaleX = mapPaths[selectedMap].scaleX;
+var scaleY = mapPaths[selectedMap].scaleY;
+var losOpacity = 0.3;
+var markers = [];
+
+// Load the height array from the CSV file
+Papa.parse(mapPaths[selectedMap].heightmap, {
+    download: true,
+    complete: function(results) {
+        heightArray = results.data;
+        heightArray = heightArray.map(function(row) {
+            return row.map(function(cell) {
+                return parseFloat(cell);
+            });
+        });
+
+        // Define the bounds of the image overlays
+        var bounds = [[0, 0], [heightArray.length, heightArray[0].length]];
+
+        // Set the view to the center of the bounds
+        var centerX = heightArray[0].length / 2;
+        var centerY = heightArray.length / 2;
+        map.setView([centerY, centerX], -2);
+
+        // Add the satellite map layer
+        var satelliteLayer = L.imageOverlay(mapPaths[selectedMap].satellite, bounds).addTo(map);
+        satelliteLayer.setOpacity(0.75);
+
+        // Add the color map layer
+        var colorLayer = L.imageOverlay(mapPaths[selectedMap].colormap, bounds).addTo(map);
+        colorLayer.setOpacity(0.75);
+
+        // Add the contour lines layer
+        var contourLayer = L.imageOverlay(mapPaths[selectedMap].contour, bounds).addTo(map);
+
+        // Update the opacity of the satellite map layer
+        document.getElementById('satellite-opacity').addEventListener('input', function (e) {
+            satelliteLayer.setOpacity(e.target.value);
+        });
+
+        // Update the opacity of the color map layer
+        document.getElementById('color-opacity').addEventListener('input', function (e) {
+            colorLayer.setOpacity(e.target.value);
+        });
+
+        // Update the opacity of the contour lines layer
+        document.getElementById('contour-opacity').addEventListener('input', function (e) {
+            contourLayer.setOpacity(e.target.value);
+        });
+
+        // Update the opacity of the lines of sight layer
+        document.getElementById('los-opacity').addEventListener('input', function (e) {
+            losOpacity = e.target.value;
+            map.fire('click');
+        });
+
+        // Display the height value when the user hovers on the map
+        map.on('mousemove', function(e) {
+            // Check if height array is loaded
+            if (heightArray.length === 0) {
+                return;
+            }
+            var x = Math.floor(e.latlng.lng);
+            var y = Math.floor(e.latlng.lat);
+            var height = heightArray[y] && heightArray[y][x] ? heightArray[y][x] : 'N/A';
+            height = Math.round(height);
+            if (clickX != null && clickY != null) {
+                var distance = Math.sqrt(Math.pow((clickX - x)*scaleX, 2) + Math.pow((clickY - y)*scaleY, 2));
+                distance = Math.round(distance);
+            } else {
+                distance = 'N/A';
+            }
+            document.getElementById('height-info').innerText = 'Height: ' + height + ' m';
+            document.getElementById('distance-info').innerText = 'Distance: ' + distance + ' m';
+        });
+
+        // Store the lines of sight
+        var linesOfSight = [];
+
+        // Draw lines of sight when the user clicks on the map
+        map.on('click', function(e) {
+            // Remove previous lines of sight
+            linesOfSight.forEach(function(line) {
+                map.removeLayer(line);
+            });
+            linesOfSight = [];
+
+            // Remove previous marker
+            map.eachLayer(function(layer) {
+                if (layer instanceof L.Marker) {
+                    // Check that marker is not in the other markers array
+                    if (!markers.includes(layer)) {
+                        map.removeLayer(layer);
+                    }
+                }
+            });
+
+            var x = Math.floor(e.latlng.lng);
+            var y = Math.floor(e.latlng.lat);
+
+            // Add a marker at the clicked location
+            var marker = L.marker([y, x]).addTo(map);
+            marker._icon.classList.add('huechange');
+            clickX = x;
+            clickY = y;
+            var maxDistance = 10000;
+            var lines = [];
+
+            for (var aimAngle = 0; aimAngle < 360; aimAngle += 1) {
+                var endPoint = calculateLineOfSight(x, y, aimAngle, maxDistance);
+                // Add all visible segments to the lines array
+                for (var i = 0; i < endPoint.length - 1; i++) {
+                    if (i % 2 == 0) {
+                        lines.push([
+                            [endPoint[i][1], endPoint[i][0]],
+                            [endPoint[i + 1][1], endPoint[i + 1][0]]
+                        ]);
+                    }
+                }
+            }
+
+            // Draw the lines of sight
+            lines.forEach(function(line) {
+                var polyline = L.polyline(line, {color: 'red', weight: 3, opacity:losOpacity}).addTo(map);
+                linesOfSight.push(polyline);
+            });
+        });
+
+        // Hide the spinner once the map is fully loaded
+        document.getElementById('spinner').style.display = 'none';
+    }
+});
+
+// Add or remove static markers
+map.on('mousedown', function(e) {
+    if (e.originalEvent.button === 1) { // Middle mouse button
+        var x = Math.floor(e.latlng.lng);
+        var y = Math.floor(e.latlng.lat);
+        var markerCounter = markers.length + 1;
+        var marker = L.marker([y, x]).addTo(map);
+        markers.push(marker);
+
+        // Add click event to remove marker
+        marker.on('click', function() {
+            map.removeLayer(marker);
+            markers = markers.filter(function(m) {
+                return m !== marker;
+            });
+        });
+    }
+});
+
+// Function to calculate the segments of line of sight from (x, y) in the direction of aimAngle
+// up to a maximum distance of maxDistance
+function calculateLineOfSight(x, y, aimAngle, maxDistance) {
+    var rad = aimAngle * Math.PI / 180;
+    var dx = Math.cos(rad);
+    var dy = Math.sin(rad);
+    var distance = 0;
+    var playerHeight = 2;
+    var step = 5;
+    var playerHeight = playerHeight + heightArray[y][x];
+    var visible = true;
+    var angle = -Math.PI / 2;
+    var blockingAngle = 0;
+    var visible_segments = [[x, y]];
+    var currentX = x;
+    var currentY = y;
+
+    while (distance < maxDistance) {
+        currentX += dy * step;
+        currentY += dx * step;
+        
+        // Use Math.floor instead of Math.round to prevent excessive snapping
+        var gridX = Math.floor(currentX);
+        var gridY = Math.floor(currentY);
+        
+        distance += step;
+
+        if (gridX < 0 || gridX >= heightArray[0].length || gridY < 0 || gridY >= heightArray.length) {
+            if (visible === true) {
+                visible_segments.push([currentX, currentY]); // Use floating-point for smoother results
+            }
+            break;
+        }
+
+        var newHeight = heightArray[gridY][gridX];
+        var newAngle = Math.atan((newHeight - playerHeight) / distance);
+        if (newAngle < angle && visible === true) {
+            visible = false;
+            blockingAngle = newAngle;
+            visible_segments.push([currentX, currentY]); // Preserve floating-point precision
+        }
+        if (newAngle > blockingAngle && visible === false) {
+            visible = true;
+            visible_segments.push([currentX, currentY]);
+        }
+        angle = newAngle;
+    }
+
+    return visible_segments;
+}
